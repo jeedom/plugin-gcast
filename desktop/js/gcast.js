43ecdd257805447fabe0706ad999ab53a1c8136e
Loïc <loic@jeedom.com>
2018-12-07 09:24:50 +0100

    ui improvements

--- desktop/js/gcast.js
+++ desktop/js/gcast.js
@@ -46,16 +46,7 @@ function addCmdToTable(_cmd) {
     jeedom.cmd.changeType($('#table_cmd tbody tr:last'), init(_cmd.subType));
 }
 
-// ABA: ajout TTSWebServer //
 function showVoiceOption(_selectValue) {
-	document.getElementById('optionpico').style.display='none'; 
-	document.getElementById('optiongoogle').style.display='none'; 
-	document.getElementById('optionttsws').style.display='none';
-	if(_selectValue=='picotts'){ //pico
-		document.getElementById('optionpico').style.display='block'; 
-	} else if(_selectValue=='ttswebserver'){ //ttswebserver
-		document.getElementById('optionttsws').style.display='block';
-	} else { //google
-		document.getElementById('optiongoogle').style.display='block'; 
-	}
+	$('.voiceOption').hide(); 
+    $('.voiceOption.'+_selectValue).show(); 
 }
